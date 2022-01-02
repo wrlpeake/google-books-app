dea1d72ff739a49e4c3644247acd90af517ae024
Will Peake <47351385+wrlpeake@users.noreply.github.com>
2022-01-02 18:18:59 +0000

    Update index.test.js
    Test package running successfully - now to add appropriate tests.

--- index.test.js
+++ index.test.js
@@ -1,2 +1,14 @@
 const index = require('./index');
 
+// What should should be tested here?
+// Options are: Whether searchResults() retuns an array
+// Whether a search term  gets five results or less
+// What are the correct results of a search term
+
+test('two plus two is four', () => {
+    expect(2 + 2).toBe(4);
+})
+
+test('does it call the function?', () => {
+    expect(index).toBeTruthy();
+});
